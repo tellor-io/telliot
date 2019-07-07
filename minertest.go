159627565dc195f882b28e0219a28281a01f1daf
themandalore <fett.nicholas@yahoo.com>
2019-07-07 08:14:45 -0400

    almost working miner

--- minertest.go
+++ minertest.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
+	"context"
+	"crypto/ecdsa"
 	"fmt"
+	"log"
 	"math/big"
-	"math/rand"
-	"strings"
 
-	solsha3 "github.com/miguelmota/go-solidity-sha3"
-	"golang.org/x/crypto/ripemd160"
-	// "crypto/sha256"
-	// "golang.org/x/crypto/ripemd160"
+	tellor "./contracts"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 const public_address string = "0xe037ec8ec9ec423826750853899394de7f024fee"
@@ -19,58 +19,57 @@ const challenge string = "a9fd1780508babd96cc82ee6f0fce53cff9127119129fb4b2a3305
 const difficulty int64 = 1
 
 func main() {
-	mine()
+	submitSolution()
 }
 
-func randInt(min int, max int) int {
-	return min + rand.Intn(max-min)
-}
-func hexaNumberToInteger(hexaString string) string {
-	// replace 0x or 0X with empty String
-	numberStr := strings.Replace(hexaString, "0x", "", -1)
-	numberStr = strings.Replace(numberStr, "0X", "", -1)
-	return numberStr
-}
-func mine() uint {
-	var i uint = 0 //
-	for i < 1 {
-		//nonce := randInt(0, 100000000000)
-		nonce := fmt.Sprintf("%02x", fmt.Sprint(9000))
-		_string := challenge + public_address[2:] + nonce
-		hash := solsha3.SoliditySHA3(
-			solsha3.Bytes32(decodeHex(_string)),
-		)
-		hasher := ripemd160.New()
-		hasher.Write([]byte(hash))
-		hash1 := hasher.Sum(nil)
-		n := sha256.Sum256([]byte(hash1))
-		q := fmt.Sprintf("%x", n)
-		p := new(big.Int)
-		p, ok := p.SetString(q, 16)
-		if !ok {
-			fmt.Println("SetString: error")
-			return 1
-		}
-		fmt.Println(p)
-		v := big.NewInt(difficulty)
-		x := new(big.Int)
-		fmt.Println(v)
-		x.Mod(p, v)
-		fmt.Println(x)
-		if x.Cmp(big.NewInt(0)) == 0 {
-			fmt.Println("Solution Found", p)
-			return i
-		}
-		fmt.Println("Solution Not Found", p)
-		i++
-	}
-	return i
-}
-func decodeHex(s string) []byte {
-	b, err := hex.DecodeString(s)
+func submitSolution() {
+	client, err := ethclient.Dial("http://localhost:8545")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	return b
+	privateKey, err := crypto.HexToECDSA("4bdc16637633fa4b4854670fbb83fa254756798009f52a1d3add27fb5f5a8e16")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	auth := bind.NewKeyedTransactor(privateKey)
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)     // in wei
+	auth.GasLimit = uint64(300000) // in units
+	auth.GasPrice = gasPrice
+
+	contractAddress := common.HexToAddress("0x9f7bC259319001166e986210d846af7C15B1f644")
+	instance, err := tellor.NewTellor(contractAddress, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var requestId uint = 9
+	var value uint = 1000
+	solution := "85000"
+
+	tx, err := instance.submitMiningSolution(auth, solution, requestId, value)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("tx sent: %s", tx.Hash().Hex()) // tx sent: 0x8d490e535678e9a24360e955d75b27ad307bdfb97a1dca51d0f3035dcee3e870
 }
