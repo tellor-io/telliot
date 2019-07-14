a514a8071a509086a6423829084aae1eec5f89a1
mdcoon <mdcoon1@yahoo.com>
2019-07-14 15:02:25 -0400

    Added tracker interface

---
+++ pkg/tracker/tracker.go
@@ -0,0 +1,10 @@
+package tracker
+
+import "context"
+
+//Tracker is the primary interface for the various tracking options
+type Tracker interface {
+	//Exec will be run as a go function. The given context will be a KeyValue context containing
+	//the client to use for tracking ops.
+	Exec(ctx context.Context) (bool, error)
+}
