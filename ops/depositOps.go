75aeb0647081763ab7d5cab632a9ae4af2c918f6
mdcoon <mdcoon1@yahoo.com>
2019-08-10 10:26:00 -0400

    Added more logging to deposit op

--- ops/depositOps.go
+++ ops/depositOps.go
@@ -2,56 +2,66 @@ package ops
 
 import (
 	"context"
-	"math/big"
-	"fmt"
-	"log"
 	"crypto/ecdsa"
+	"fmt"
+	"math/big"
+
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/tellor-io/TellorMiner/rpc"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/tellor-io/TellorMiner/config"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	tellorCommon "github.com/tellor-io/TellorMiner/common"
-	tellor1 "github.com/tellor-io/TellorMiner/contracts1"
+	"github.com/tellor-io/TellorMiner/config"
 	tellor "github.com/tellor-io/TellorMiner/contracts"
+	tellor1 "github.com/tellor-io/TellorMiner/contracts1"
+	"github.com/tellor-io/TellorMiner/rpc"
+	"github.com/tellor-io/TellorMiner/util"
 )
 
 /**
  * This is the operational deposit component. Its purpose is to deposit Tellor Tokens so you can mine
  */
 
+var (
+	depositLog = util.NewLogger("ops", "DepositOp")
+)
 
- func Deposit(ctx context.Context) (error) {
+func Deposit(ctx context.Context) error {
 	cfg, err := config.GetConfig()
 	if err != nil {
+		depositLog.Error("Problem getting config: %+v", err)
 		return err
 	}
 	client := ctx.Value(tellorCommon.ClientContextKey).(rpc.ETHClient)
 
 	privateKey, err := crypto.HexToECDSA(cfg.PrivateKey)
 	if err != nil {
+		depositLog.Error("Problem getting private key: %+v", err)
 		return err
 	}
 
 	publicKey := privateKey.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
 	if !ok {
+		depositLog.Error("Problem extract public key")
 		return fmt.Errorf("error casting public key to ECDSA")
 	}
 
 	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
 	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
 	if err != nil {
+		depositLog.Error("Problem getting pending nonce: %+v", err)
 		return err
 	}
 
 	gasPrice, err := client.SuggestGasPrice(context.Background())
 	if err != nil {
+		depositLog.Error("Problem getting gas price: %+v", err)
 		return err
 	}
 
 	balance, err := client.BalanceAt(context.Background(), fromAddress, nil)
 	if err != nil {
+		depositLog.Error("Problem getting balance: %+v", err)
 		return err
 	}
 
@@ -59,6 +69,7 @@ import (
 	cost.Mul(gasPrice, big.NewInt(700000))
 	if balance.Cmp(cost) < 0 {
 		//FIXME: notify someone that we're out of funds!
+		depositLog.Error("Insufficient funds")
 		return fmt.Errorf("Insufficient funds to send transaction: %v < %v", balance, cost)
 	}
 
@@ -68,23 +79,24 @@ import (
 	auth.GasLimit = uint64(3000000) // in units
 	auth.GasPrice = gasPrice
 
-
 	contractAddress := common.HexToAddress(cfg.ContractAddress)
 
 	instance, err := tellor.NewTellorMaster(contractAddress, client)
 	if err != nil {
-		log.Fatal(err)
+		depositLog.Error("Problem creating contract: %+v", err)
+		//log.Fatal(err)
 		return err
 	}
 
-	balance, err = instance.BalanceOf(nil,fromAddress)
-	log.Printf("Balance: %v\n", balance)
+	balance, err = instance.BalanceOf(nil, fromAddress)
+	depositLog.Info("Balance: %v\n", balance)
 	if err != nil {
-		log.Fatal(err)
+		depositLog.Error("Problem getting balance: %+v", err)
 		return err
 	}
 	amt := big.NewInt(1000)
-	if balance.Cmp(amt) < 0{
+	if balance.Cmp(amt) < 0 {
+		depositLog.Error("Insufficient token balance: %+v", balance)
 		fmt.Println("You must have the amount you want to send")
 		return nil
 	}
@@ -93,11 +105,11 @@ import (
 
 	tx, err := instance2.DepositStake(auth)
 	if err != nil {
-		log.Fatal(err)
+		depositLog.Error("Could not deposit stake: %+v", err)
 		return err
 	}
 
-	fmt.Printf("tx sent: %s", tx.Hash().Hex())
+	depositLog.Info("tx sent: %s", tx.Hash().Hex())
 
 	return nil
 }
