d81d180db65cf5e4d023a1df38b26cb785fb1958
themandalore <fett.nicholas@yahoo.com>
2019-08-12 11:29:29 -0400

    binary?

--- runTestMain.sh
+++ runTestMain.sh
@@ -9,12 +9,12 @@ CFG5=`pwd`/config5.json
 CFG6=`pwd`/config6.json
 LOG=`pwd`/loggingConfig.json
 echo "Starting TellorMiner main"
-nohup go run . -psrPath="${PSR}" -config="${CFG6}" -logConfig="${LOG}" "$@" &
-nohup go run . -psrPath="${PSR}" -config="${CFG1}" -logConfig="${LOG}" "$@" &
-nohup go run . -psrPath="${PSR}" -config="${CFG2}" -logConfig="${LOG}" "$@" &
-nohup go run . -psrPath="${PSR}" -config="${CFG3}" -logConfig="${LOG}" "$@" &
-nohup go run . -psrPath="${PSR}" -config="${CFG4}" -logConfig="${LOG}" "$@" &
-nohup go run . -psrPath="${PSR}" -config="${CFG5}" -logConfig="${LOG}" "$@" 
+go run . -psrPath="${PSR}" -config="${CFG6}" -logConfig="${LOG}" "$@" &
+go run . -psrPath="${PSR}" -config="${CFG1}" -logConfig="${LOG}" "$@" &
+go run . -psrPath="${PSR}" -config="${CFG2}" -logConfig="${LOG}" "$@" &
+go run . -psrPath="${PSR}" -config="${CFG3}" -logConfig="${LOG}" "$@" &
+go run . -psrPath="${PSR}" -config="${CFG4}" -logConfig="${LOG}" "$@" &
+go run . -psrPath="${PSR}" -config="${CFG5}" -logConfig="${LOG}" "$@" 
 #go run . -miner -dataServer -psrPath=C:/company/code/go/src/github.com/tellor-io/TellorMiner/psr.json -config=C:/company/code/go/src/github.com/tellor-io/TellorMiner/config3.json -logConfig=C:/company/code/go/src/github.com/tellor-io/TellorMiner/loggingConfig.json
 #go run ./main.go -deposit -config=./config6.json -psrPath=./psr2.json -logConfig=./loggingConfig.json
 #go run ./main.go -transfer -to=0xe0d7bae200f0994b11423e8be8f386060bbdd808 -amount=1000000000000000000000 -config=./config3.json -psrPath=./psr2.json -logConfig=./loggingConfig.json
