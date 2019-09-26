996410060eecc5079bff5c6dfd70e57ae8140fa5
themandalore <fett.nicholas@yahoo.com>
2019-09-26 08:59:29 -0400

    psrUpdate

--- runMain.sh
+++ runMain.sh
@@ -19,3 +19,11 @@ go run . -psrPath="${PSR}" -config="${CFG}" -logConfig="${LOG}" "$@"
 
 # To test:
 # go run ./main.go -withdraw -config=./myconfig.json -psrPath=./psr.json -logConfig=./loggingConfig.json
+
+
+# go run ./main.go -deposit -config=./myconfig.json -psrPath=./psr.json -logConfig=./loggingConfig.json
+# go run ./main.go -transfer -to=0x538d278e05a35c96bcdca1039e92c65b994256a0 -amount=1 -config=./myconfig.json -psrPath=./psr.json -logConfig=./loggingConfig.json
+# go run ./main.go -approve -to=0x538d278e05a35c96bcdca1039e92c65b994256a0 -amount=1 -config=./myconfig.json -psrPath=./psr.json -logConfig=./loggingConfig.json
+# go run ./main.go -vote -disputeId=2 -supportsDispute=false -config=./myconfig.json -psrPath=./psr.json -logConfig=./loggingConfig.json
+# go run ./main.go -dispute -requestId=14 -timestamp=1568779500 -minerIndex=2 -config=./myconfig.json -psrPath=./psr.json -logConfig=./loggingConfig.json
+# go run ./main.go -requestStakingWithdraw -config=./myconfig.json -psrPath=./psr.json -logConfig=./loggingConfig.json
