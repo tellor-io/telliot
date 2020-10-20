3fb33041d05c7ef49d8995b4cadce910039cc656
JG Carvalho <jg@jgcarv.com>
2020-10-20 16:14:40 -0300

    added logger to test

--- pkg/ops/dataServerOps_test.go
+++ pkg/ops/dataServerOps_test.go
@@ -8,21 +8,31 @@ import (
 	"testing"
 	"time"
 
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
 	"github.com/tellor-io/TellorMiner/pkg/testutil"
 )
 
+func setupLogger() log.Logger {
+	lvl := level.AllowInfo()
+
+	logger := log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+	logger = level.NewFilter(logger, lvl)
+
+	return log.With(logger, "ts", log.DefaultTimestampUTC, "caller", log.DefaultCaller)
+}
 func TestDataServerOps(t *testing.T) {
 
 	exitCh := make(chan os.Signal)
-
+	logger := setupLogger()
 	ctx, _, cleanup := testutil.CreateContext(t)
 	defer t.Cleanup(cleanup)
 
-	ops, err := CreateDataServerOps(ctx, exitCh)
+	ops, err := CreateDataServerOps(ctx, logger, exitCh)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if err := ops.Start(ctx); err != nil {
+	if err := ops.Start(ctx, logger); err != nil {
 		t.Fatal("error starting the data server", err)
 	}
 	time.Sleep(2 * time.Second)
