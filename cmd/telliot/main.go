ea25ff9d7e4461cc45b1e93947f7cb4955051ce4
JG Carvalho <jg@jgcarv.com>
2020-12-04 14:25:35 -0300

    finished basic app structure

--- cmd/telliot/main.go
+++ cmd/telliot/main.go
@@ -154,19 +154,26 @@ func AddDBToCtx(remote bool) error {
 	"fmt"
 	"context"
 	"crypto/ecdsa"
+	"math/big"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
 
 	"github.com/alecthomas/kong"
 <<<<<<< HEAD
 =======
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/pkg/errors"
 	tellorCommon "github.com/tellor-io/telliot/pkg/common"
 	"github.com/tellor-io/telliot/pkg/config"
 	"github.com/tellor-io/telliot/pkg/contracts/getter"
 	"github.com/tellor-io/telliot/pkg/contracts/tellor"
+	"github.com/tellor-io/telliot/pkg/db"
 	"github.com/tellor-io/telliot/pkg/ops"
 	"github.com/tellor-io/telliot/pkg/rpc"
 	"github.com/tellor-io/telliot/pkg/util"
@@ -251,35 +258,35 @@ func setup() (rpc.ETHClient, tellorCommon.Contract, tellorCommon.Account, error)
 	return nil, tellorCommon.Contract{}, tellorCommon.Account{}, nil
 }
 
-// func AddDBToCtx(remote bool) error {
-// 	cfg := config.GetConfig()
-// 	// Create a db instance
-// 	os.RemoveAll(cfg.DBFile)
-// 	DB, err := db.Open(cfg.DBFile)
-// 	if err != nil {
-// 		return errors.Wrapf(err, "opening DB instance")
-// 	}
+func AddDBToCtx(remote bool) error {
+	cfg := config.GetConfig()
+	// Create a db instance
+	os.RemoveAll(cfg.DBFile)
+	DB, err := db.Open(cfg.DBFile)
+	if err != nil {
+		return errors.Wrapf(err, "opening DB instance")
+	}
 
-// 	var dataProxy db.DataServerProxy
-// 	if remote {
-// 		proxy, err := db.OpenRemoteDB(DB)
-// 		if err != nil {
-// 			return errors.Wrapf(err, "open remote DB instance")
+	var dataProxy db.DataServerProxy
+	if remote {
+		proxy, err := db.OpenRemoteDB(DB)
+		if err != nil {
+			return errors.Wrapf(err, "open remote DB instance")
 
-// 		}
-// 		dataProxy = proxy
-// 	} else {
-// 		proxy, err := db.OpenLocalProxy(DB)
-// 		if err != nil {
-// 			return errors.Wrapf(err, "opening local DB instance:")
+		}
+		dataProxy = proxy
+	} else {
+		proxy, err := db.OpenLocalProxy(DB)
+		if err != nil {
+			return errors.Wrapf(err, "opening local DB instance:")
 
-// 		}
-// 		dataProxy = proxy
-// 	}
-// 	ctx = context.WithValue(ctx, tellorCommon.DataProxyKey, dataProxy)
-// 	ctx = context.WithValue(ctx, tellorCommon.DBContextKey, DB)
-// 	return nil
-// }
+		}
+		dataProxy = proxy
+	}
+	ctx = context.WithValue(ctx, tellorCommon.DataProxyKey, dataProxy)
+	ctx = context.WithValue(ctx, tellorCommon.DBContextKey, DB)
+	return nil
+}
 
 // var GitTag string
 // var GitHash string
@@ -399,156 +406,140 @@ func setup() (rpc.ETHClient, tellorCommon.Contract, tellorCommon.Account, error)
 // 	}
 // }
 
-// func newDisputeCmd(cmd *cli.Cmd) {
-// 	requestID := EthereumInt{}
-// 	timestamp := EthereumInt{}
-// 	minerIndex := EthereumInt{}
-// 	cmd.VarArg("REQUEST_ID", &requestID, "request id")
-// 	cmd.VarArg("TIMESTAMP", &timestamp, "timestamp")
-// 	cmd.VarArg("MINER_INDEX", &minerIndex, "miner to dispute (0-4)")
-// 	cmd.Action = func() {
-// 		ExitOnError(ops.Dispute(ctx, clt, cont, acc, requestID.Int, timestamp.Int, minerIndex.Int), "new dipsute")
-// 	}
-// }
+func (m mineCmd) Run(logger log.Logger) error {
+	// Create os kill sig listener.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	exitChannels := make([]*chan os.Signal, 0)
 
-// func mineCmd(logSetup func(string) log.Logger, logLevel *string) func(*cli.Cmd) {
-// 	return func(cmd *cli.Cmd) {
-// 		remoteDS := cmd.BoolOpt("remote r", false, "connect to remote dataserver")
-// 		cmd.Action = func() {
-// 			logger := logSetup(*logLevel)
-// 			// Create os kill sig listener.
-// 			c := make(chan os.Signal, 1)
-// 			signal.Notify(c, os.Interrupt)
-// 			exitChannels := make([]*chan os.Signal, 0)
+	cfg := config.GetConfig()
+	var ds *ops.DataServerOps
+	if !cfg.EnablePoolWorker {
+		var err error
+		err = AddDBToCtx(m.remote)
+		if err != nil {
+			return errors.Wrapf(err, "initializing database")
+		}
+		if !m.remote {
+			ch := make(chan os.Signal)
+			exitChannels = append(exitChannels, &ch)
 
-// 			cfg := config.GetConfig()
-// 			var ds *ops.DataServerOps
-// 			if !cfg.EnablePoolWorker {
-// 				ExitOnError(AddDBToCtx(*remoteDS), "initializing database")
-// 				if !*remoteDS {
-// 					ch := make(chan os.Signal)
-// 					exitChannels = append(exitChannels, &ch)
+			ds, err = ops.CreateDataServerOps(ctx, logger, ch)
+			if err != nil {
+				return errors.Wrapf(err, "creating data server")
+			}
+			// Start and wait for it to be ready.
+			if err := ds.Start(ctx); err != nil {
+				return errors.Wrapf(err, "starting data server")
+			}
+			<-ds.Ready()
+		}
+	}
+	// Start miner
+	DB := ctx.Value(tellorCommon.DataProxyKey).(db.DataServerProxy)
+	v, err := DB.Get(db.DisputeStatusKey)
+	if err != nil {
+		level.Warn(logger).Log("msg", "getting dispute status. Check if staked")
+	}
+	status, _ := hexutil.DecodeBig(string(v))
+	if status.Cmp(big.NewInt(1)) != 0 {
+		return errors.New("miner is not able to mine with current status")
+	}
+	ch := make(chan os.Signal)
+	exitChannels = append(exitChannels, &ch)
+	miner, err := ops.CreateMiningManager(logger, ch, cfg, DB)
+	if err != nil {
+		return errors.Wrapf(err, "creating miner")
+	}
+	go func() {
+		miner.Start(ctx)
+	}()
 
-// 					var err error
-// 					ds, err = ops.CreateDataServerOps(ctx, logger, ch)
-// 					if err != nil {
-// 						ExitOnError(err, "creating data server")
-// 					}
-// 					// Start and wait for it to be ready.
-// 					if err := ds.Start(ctx); err != nil {
-// 						ExitOnError(err, "starting data server")
-// 					}
-// 					<-ds.Ready()
-// 				}
-// 			}
-// 			// Start miner
-// 			DB := ctx.Value(tellorCommon.DataProxyKey).(db.DataServerProxy)
-// 			v, err := DB.Get(db.DisputeStatusKey)
-// 			if err != nil {
-// 				level.Warn(logger).Log("msg", "getting dispute status. Check if staked")
-// 			}
-// 			status, _ := hexutil.DecodeBig(string(v))
-// 			if status.Cmp(big.NewInt(1)) != 0 {
-// 				ExitOnError(errors.New("miner is not able to mine with current status"), "checking miner")
-// 			}
-// 			ch := make(chan os.Signal)
-// 			exitChannels = append(exitChannels, &ch)
-// 			miner, err := ops.CreateMiningManager(logger, ch, cfg, DB)
-// 			if err != nil {
-// 				ExitOnError(err, "creating miner")
-// 			}
-// 			go func() {
-// 				miner.Start(ctx)
-// 			}()
+	// Wait for kill sig.
+	<-c
+	// Then notify exit channels.
+	for _, ch := range exitChannels {
+		*ch <- os.Interrupt
+	}
+	cnt := 0
+	start := time.Now()
+	for {
+		cnt++
+		dsStopped := false
+		minerStopped := false
 
-// 			// Wait for kill sig.
-// 			<-c
-// 			// Then notify exit channels.
-// 			for _, ch := range exitChannels {
-// 				*ch <- os.Interrupt
-// 			}
-// 			cnt := 0
-// 			start := time.Now()
-// 			for {
-// 				cnt++
-// 				dsStopped := false
-// 				minerStopped := false
+		if ds != nil {
+			dsStopped = !ds.Running
+		} else {
+			dsStopped = true
+		}
 
-// 				if ds != nil {
-// 					dsStopped = !ds.Running
-// 				} else {
-// 					dsStopped = true
-// 				}
+		if miner != nil {
+			minerStopped = !miner.Running
+		} else {
+			minerStopped = true
+		}
 
-// 				if miner != nil {
-// 					minerStopped = !miner.Running
-// 				} else {
-// 					minerStopped = true
-// 				}
+		if !dsStopped && !minerStopped && cnt > 60 {
+			level.Warn(logger).Log("msg", "taking longer than expected to stop operations", "waited", time.Since(start))
+		} else if dsStopped && minerStopped {
+			break
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+	level.Info(logger).Log("msg", "main shutdown complete")
+	return nil
+}
 
-// 				if !dsStopped && !minerStopped && cnt > 60 {
-// 					level.Warn(logger).Log("msg", "taking longer than expected to stop operations", "waited", time.Since(start))
-// 				} else if dsStopped && minerStopped {
-// 					break
-// 				}
-// 				time.Sleep(500 * time.Millisecond)
-// 			}
-// 			level.Info(logger).Log("msg", "main shutdown complete")
-// 		}
-// 	}
-// }
+func (d dataserverCmd) Run(logger log.Logger) error {
+	// Create os kill sig listener.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
 
-// func dataserverCmd(logSetup func(string) log.Logger, logLevel *string) func(*cli.Cmd) {
-// 	return func(cmd *cli.Cmd) {
-// 		cmd.Action = func() {
-// 			logger := logSetup(*logLevel)
-// 			// Create os kill sig listener.
-// 			c := make(chan os.Signal, 1)
-// 			signal.Notify(c, os.Interrupt)
+	var ds *ops.DataServerOps
+	var err error
+	err = AddDBToCtx(true)
+	if err != nil {
+		return errors.Wrapf(err, "initializing database")
+	}
+	ch := make(chan os.Signal)
+	ds, err = ops.CreateDataServerOps(ctx, logger, ch)
+	if err != nil {
+		return errors.Wrapf(err, "creating data server")
+	}
+	// Start and wait for it to be ready
+	if err := ds.Start(ctx); err != nil {
+		return errors.Wrapf(err, "starting data server")
+	}
+	<-ds.Ready()
 
-// 			var ds *ops.DataServerOps
-// 			ExitOnError(AddDBToCtx(true), "initializing database")
-// 			ch := make(chan os.Signal)
-// 			var err error
-// 			ds, err = ops.CreateDataServerOps(ctx, logger, ch)
-// 			if err != nil {
-// 				ExitOnError(err, "creating data server")
-// 			}
-// 			// Start and wait for it to be ready
-// 			if err := ds.Start(ctx); err != nil {
-// 				ExitOnError(err, "starting data server")
-// 			}
-// 			<-ds.Ready()
+	// Wait for kill sig.
+	<-c
+	// Notify exit channels.
+	ch <- os.Interrupt
 
-// 			// Wait for kill sig.
-// 			<-c
-// 			// Notify exit channels.
-// 			ch <- os.Interrupt
+	cnt := 0
+	start := time.Now()
+	for {
+		cnt++
+		dsStopped := false
 
-// 			cnt := 0
-// 			start := time.Now()
-// 			for {
-// 				cnt++
-// 				dsStopped := false
+		if ds != nil {
+			dsStopped = !ds.Running
+		} else {
+			dsStopped = true
+		}
 
-// 				if ds != nil {
-// 					dsStopped = !ds.Running
-// 				} else {
-// 					dsStopped = true
-// 				}
-
-// 				if !dsStopped && cnt > 60 {
-// 					level.Warn(logger).Log("msg", "taking longer than expected to stop operations", "waited", time.Since(start))
-// 				} else if dsStopped {
-// 					break
-// 				}
-// 				time.Sleep(500 * time.Millisecond)
-// 			}
-// 			level.Info(logger).Log("msg", "main shutdown complete")
-// 		}
-
-// 	}
-// }
+		if !dsStopped && cnt > 60 {
+			level.Warn(logger).Log("msg", "taking longer than expected to stop operations", "waited", time.Since(start))
+		} else if dsStopped {
+			break
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+	level.Info(logger).Log("msg", "main shutdown complete")
+	return nil
+}
 
 // func main() {
 // 	// Programming is easy. Just create an App() and run it!!!!!
@@ -625,12 +616,44 @@ func (c logConfigPath) BeforeApply(ctx *kong.Context) error {
 }
 
 type newDisputeCmd struct {
+	requestID  string `arg required `
+	minerIndex string `arg required `
+	timestamp  string `arg required `
+}
+
+func (n newDisputeCmd) Run(logger log.Logger, client rpc.ETHClient, contract tellorCommon.Contract, account tellorCommon.Account) error {
+	requestID := EthereumInt{}
+	err := requestID.Set(n.requestID)
+	if err != nil {
+		return errors.Wrapf(err, "parsing argument")
+	}
+	timestamp := EthereumInt{}
+	err = timestamp.Set(n.timestamp)
+	if err != nil {
+		return errors.Wrapf(err, "parsing argument")
+	}
+	minerIndex := EthereumInt{}
+	err = minerIndex.Set(n.minerIndex)
+	if err != nil {
+		return errors.Wrapf(err, "parsing argument")
+	}
+	return ops.Dispute(ctx, client, contract, account, requestID.Int, timestamp.Int, minerIndex.Int)
 }
 
 type voteCmd struct {
-	disputeId int  `arg required`
-	support   bool `arg required`
+	disputeId string `arg required`
+	support   bool   `arg required`
 }
+
+func (v voteCmd) Run(client rpc.ETHClient, contract tellorCommon.Contract, account tellorCommon.Account) error {
+	disputeID := EthereumInt{}
+	err := disputeID.Set(v.disputeId)
+	if err != nil {
+		return errors.Wrapf(err, "parsing argument")
+	}
+	return ops.Vote(ctx, client, contract, account, disputeID.Int, v.support)
+}
+
 type stakeCmd struct {
 	Operation string `arg required`
 }
@@ -676,32 +699,46 @@ type tokenCmd struct {
 
 func (b *balanceCmd) Run(client rpc.ETHClient, contract tellorCommon.Contract) error {
 	addr := ETHAddress{}
-	addr.Set(b.Address)
+	var err error
 	if b.Address == "" {
-		addr.Set(contract.Address.String())
+		err = addr.Set(contract.Address.String())
+		if err != nil {
+			return errors.Wrapf(err, "parsing argument")
+		}
 	} else {
-		addr.Set(b.Address)
+		err = addr.Set(b.Address)
+		if err != nil {
+			return errors.Wrapf(err, "parsing argument")
+		}
 	}
 	return ops.Balance(ctx, client, contract.Getter, addr.addr)
 }
 
 func (c *transferCmd) Run(logger log.Logger, client rpc.ETHClient, contract tellorCommon.Contract, account tellorCommon.Account) error {
 	address := ETHAddress{}
-	address.Set(c.Address)
-
+	err := address.Set(c.Address)
+	if err != nil {
+		return errors.Wrapf(err, "parsing argument")
+	}
 	amount := EthereumInt{}
-	amount.Set(c.Amount)
-
+	err = amount.Set(c.Amount)
+	if err != nil {
+		return errors.Wrapf(err, "parsing argument")
+	}
 	return ops.Transfer(ctx, logger, client, contract, account, address.addr, amount.Int)
 }
 
 func (c *approveCmd) Run(logger log.Logger, client rpc.ETHClient, contract tellorCommon.Contract, account tellorCommon.Account) error {
 	address := ETHAddress{}
-	address.Set(c.Address)
-
+	err := address.Set(c.Address)
+	if err != nil {
+		return errors.Wrapf(err, "parsing argument")
+	}
 	amount := EthereumInt{}
-	amount.Set(c.Amount)
-
+	err = amount.Set(c.Amount)
+	if err != nil {
+		return errors.Wrapf(err, "parsing argument")
+	}
 	return ops.Approve(ctx, logger, client, contract, account, address.addr, amount.Int)
 }
 
