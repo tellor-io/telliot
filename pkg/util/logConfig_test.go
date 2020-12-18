2c8da2d78a6f4a23a014b6a7fda1aca0b1fa7816
JG Carvalho <jg@jgcarv.com>
2020-12-18 10:46:53 -0300

    formatting

--- pkg/util/logConfig_test.go
+++ pkg/util/logConfig_test.go
@@ -11,19 +11,19 @@ import (
 )
 
 func TestLogConfig(t *testing.T) {
-	var defaultEntries = map[string]string {
-		"config.Config": "INFO",
-		"db.DB": "INFO",
-		"rpc.client": "INFO",
-		"rpc.ABICodec": "INFO",
-		"rpc.mockClient": "INFO",
-		"tracker.Top50Tracker": "INFO",
+	var defaultEntries = map[string]string{
+		"config.Config":            "INFO",
+		"db.DB":                    "INFO",
+		"rpc.client":               "INFO",
+		"rpc.ABICodec":             "INFO",
+		"rpc.mockClient":           "INFO",
+		"tracker.Top50Tracker":     "INFO",
 		"tracker.FetchDataTracker": "INFO",
-		"pow.MiningWorker-0:": "INFO",
-		"pow.MiningWorker-1:": "INFO",
-		"pow.MiningTasker-0:": "INFO",
-		"pow.MiningTasker-1:": "INFO",
-		"tracker.PSRTracker": "INFO",
+		"pow.MiningWorker-0:":      "INFO",
+		"pow.MiningWorker-1:":      "INFO",
+		"pow.MiningTasker-0:":      "INFO",
+		"pow.MiningTasker-1:":      "INFO",
+		"tracker.PSRTracker":       "INFO",
 	}
 	err := SetupLoggingConfig(defaultEntries)
 	testutil.Ok(t, err)
