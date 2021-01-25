75aa20c20355916708dbb0c0f9caf8c5c3236d86
Homayoun <hhio618@gmail.com>
2021-01-25 11:52:02 +0330

    Auto detect network for blockchain trackers (#383)
    The index.json file format now requires specifying mainnet and rinkeby contract addresses for the blockchain trackers

--- pkg/util/ethereum.go
+++ pkg/util/ethereum.go
@@ -13,15 +13,41 @@ import (
 var ethAddressRE *regexp.Regexp = regexp.MustCompile("^0x[0-9a-fA-F]{40}$")
 
 // ValidateAddress checks if an ethereum URL is valid?
-func ValidateAddress(ethereumURL string) (string, error) {
-	// Get ethereum address from ethereum URL.
-	// https://eips.ethereum.org/EIPS/eip-681
-	parts := strings.Split(ethereumURL, ":")
-	if len(parts) != 2 {
-		return "", errors.New("invalid ethereum URL")
+func ValidateAddress(address string) error {
+	if match := ethAddressRE.MatchString(address); !match {
+		return errors.New("invalid ethereum address")
 	}
-	if match := ethAddressRE.MatchString(parts[1]); !match {
-		return "", errors.New("invalid ethereum address")
+	return nil
+}
+
+// GetAddressForNetwork returns an ethereum address based on ethereum node network id.
+func GetAddressForNetwork(addresses string, networkID int64) (string, error) {
+	// Parse addresses to a map.
+	networkToAddress := make(map[string]string)
+	_addresses := strings.Split(addresses, ",")
+	for _, address := range _addresses {
+		parts := strings.Split(strings.TrimSpace(address), ":")
+		if len(parts) != 2 {
+			return "", errors.New("malformed ethereum <network:address> string")
+		}
+		if err := ValidateAddress(parts[1]); err != nil {
+			return "", err
+		}
+		networkToAddress[parts[0]] = parts[1]
 	}
-	return parts[1], nil
+
+	switch networkID {
+	case 1:
+		if val, ok := networkToAddress["Mainnet"]; ok {
+			return val, nil
+		}
+		return "", errors.New("address for the Mainnet network not found in the address list")
+	case 4:
+		if val, ok := networkToAddress["Rinkeby"]; ok {
+			return val, nil
+		}
+		return "", errors.New("address for the Rinkeby network not found in the address list")
+	default:
+		return "", errors.New("unhandled network id")
+	}
 }
