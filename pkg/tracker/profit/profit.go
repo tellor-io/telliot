8ae6f814a259731bd5a4277d6ed8f40d93577b0f
Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>
2021-04-05 13:17:36 +0300

    added the cost metric
    Signed-off-by: Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>

--- pkg/tracker/profit/profit.go
+++ pkg/tracker/profit/profit.go
@@ -37,8 +37,6 @@ type ProfitTracker struct {
 	stop             context.CancelFunc
 	addrs            []common.Address
 
-	ticker *time.Ticker
-
 	cacheTXsProfit gcache.Cache
 	cacheTXsCost   gcache.Cache
 
@@ -76,13 +74,19 @@ func NewProfitTracker(
 		cacheTXsProfit: gcache.New(50).LRU().Build(),
 		cacheTXsCost:   gcache.New(50).LRU().Build(),
 
-		ticker: time.NewTicker(time.Second),
-
 		submitProfit: promauto.NewGaugeVec(prometheus.GaugeOpts{
 			Namespace: "telliot",
 			Subsystem: ComponentName,
 			Name:      "submit_profit",
 			Help:      "Accumulated TRB amount from rewards for all registered addresses",
+		},
+			[]string{"addr"},
+		),
+		submitCost: promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "telliot",
+			Subsystem: ComponentName,
+			Name:      "submit_cost",
+			Help:      "Accumulated ETH cost from the submits for all registered addresses",
 		},
 			[]string{"addr"},
 		),
@@ -107,18 +111,26 @@ func NewProfitTracker(
 
 func (self *ProfitTracker) Start() error {
 	level.Info(self.logger).Log("msg", "starting")
-	defer self.ticker.Stop()
 
 	for _, addr := range self.addrs {
-		amountTRB, err := self.getTRBBalance(addr)
+		balance, err := self.getTRBBalance(addr)
 		if err != nil {
 			level.Error(self.logger).Log("msg", "getting initial TRB balance", "addr", addr.String(), "err", err)
 		}
-		level.Info(self.logger).Log("msg", "initial TRB balance", "addr", addr.String(), "balance", amountTRB)
-		self.balanceTRB.With(prometheus.Labels{"addr": addr.String()}).(prometheus.Gauge).Set(amountTRB)
+		level.Info(self.logger).Log("msg", "initial TRB balance", "addr", addr.String(), "balance", balance)
+		self.balanceTRB.With(prometheus.Labels{"addr": addr.String()}).(prometheus.Gauge).Set(balance)
 	}
 
-	// go self.monitorCost()
+	for _, addr := range self.addrs {
+		balance, err := self.getETHBalance(addr)
+		if err != nil {
+			level.Error(self.logger).Log("msg", "getting initial ETH balance", "addr", addr.String(), "err", err)
+		}
+		level.Info(self.logger).Log("msg", "initial ETH balance", "addr", addr.String(), "balance", balance)
+		self.balanceETH.With(prometheus.Labels{"addr": addr.String()}).(prometheus.Gauge).Set(balance)
+	}
+
+	go self.monitorCost()
 	go self.monitorReward()
 
 	<-self.ctx.Done()
@@ -129,156 +141,19 @@ func (self *ProfitTracker) Stop() {
 	self.stop()
 }
 
-// func (self *ProfitTracker) monitorCost() {
-// 	var err error
-// 	ticker := time.NewTicker(time.Second)
-// 	defer ticker.Stop()
-
-// 	var sub event.Subscription
-// 	events := make(chan *tellor.ITellorNonceSubmitted)
-
-// 	for {
-// 		sub, err = self.nonceSubmittedSub(events)
-// 		if err != nil {
-// 			level.Error(self.logger).Log("msg", "initial subscribing to NonceSubmitted events failed")
-// 			select {
-// 			case <-ticker.C:
-// 				continue
-// 			case <-self.ctx.Done():
-// 				return
-// 			}
-// 		}
-// 		break
-// 	}
-
-// 	for {
-// 		select {
-// 		case <-self.ctx.Done():
-// 			return
-// 		case err := <-sub.Err():
-// 			if err != nil {
-// 				level.Error(self.logger).Log(
-// 					"msg",
-// 					"NonceSubmitted subscription error",
-// 					"err", err)
-// 			}
-
-// 			for {
-// 				sub, err = self.nonceSubmittedSub(events)
-// 				if err != nil {
-// 					level.Error(self.logger).Log("msg", "re-subscribing to NonceSubmitted events failed")
-// 					select {
-// 					case <-ticker.C:
-// 						continue
-// 					case <-self.ctx.Done():
-// 						return
-// 					}
-// 				}
-// 				break
-// 			}
-// 			level.Info(self.logger).Log("msg", "re-subscribed to NonceSubmitted events")
-// 		case event := <-events:
-// 			logger := log.With(self.logger, "addr", event.Miner.String()[:6], "tx", event.Raw.TxHash)
-// 			go self.setCostWhenConfirmed(logger, event)
-// 			fmt.Println("vLog cost", event)
-
-// 			amount, err := self.getETHBalance()
-// 			if err != nil {
-// 				level.Error(self.logger).Log("msg", "getting ETH balance", "err", err)
-// 			} else {
-// 				level.Info(self.logger).Log("msg", "ETH balance", "amount", amount)
-
-// 			}
-// 		}
-// 	}
-// }
-
-// func (self *ProfitTracker) setCostWhenConfirmed(logger log.Logger, event *tellor.ITellorNonceSubmitted) {
-// 	if event.Raw.Removed { // Ignore remove events due to reorg.
-// 		level.Debug(logger).Log("msg", "reorg event ignored")
-// 		return
-// 	}
-// 	receipt, err := self.waitMined(logger, event.Raw.TxHash)
-// 	if err != nil {
-// 		level.Error(logger).Log("msg", "wait confirmation for cost event", "err", err)
-// 		return
-// 	}
-// 	tx, _, err := self.client.TransactionByHash(self.ctx, event.Raw.TxHash)
-// 	if err != nil {
-// 		level.Error(logger).Log("msg", "get transaction by hash", "err", err)
-// 		return
-// 	}
-// 	cost, _ := big.NewFloat(0).Mul(big.NewFloat(float64(tx.GasPrice().Int64())), big.NewFloat(float64(receipt.GasUsed))).Float64()
-// 	level.Debug(logger).Log("msg", "adding cost", "amount", cost/1e18)
-// 	self.submitCost.With(prometheus.Labels{"addr": event.Miner.String()[:6]}).(prometheus.Gauge).Add(cost / 1e18)
-// }
-
-func (self *ProfitTracker) setProfitWhenConfirmed(ctx context.Context, logger log.Logger, event *tellor.ITellorTransferred) {
-	for {
-		receipt, err := self.client.TransactionReceipt(self.ctx, event.Raw.TxHash)
-		if err != nil {
-			level.Error(logger).Log("msg", "receipt retrieval failed", "err", err)
-		} else if receipt != nil {
-			if receipt.Status != types.ReceiptStatusSuccessful {
-				level.Error(logger).Log("msg", "event status not success so no profit added", "status", receipt.Status)
-				return
-			}
-
-			trb, _ := big.NewFloat(float64(event.Value.Int64())).Float64()
-			trb = trb / 1e18
-			level.Debug(logger).Log("msg", "adding profit", "amount", trb)
-			self.submitProfit.With(prometheus.Labels{"addr": event.To.String()}).(prometheus.Gauge).Add(trb)
-
-			if err := self.cacheTXsProfit.Set(txID(event), trb); err != nil {
-				level.Error(self.logger).Log("msg", "adding profit amount to the cache", "err", err)
-			}
-
-			amountTRB, err := self.getTRBBalance(event.To)
-			if err != nil {
-				level.Error(self.logger).Log("msg", "getting TRB balance", "err", err)
-				return
-			}
-			level.Debug(logger).Log("msg", "new TRB balance", "balance", amountTRB)
-			self.balanceTRB.With(prometheus.Labels{"addr": event.To.String()}).(prometheus.Gauge).Set(amountTRB)
-			return
-		}
-
-		level.Debug(logger).Log("msg", "transaction not yet mined")
-
-		select {
-		case <-ctx.Done():
-			level.Debug(logger).Log("msg", "transaction confirmation check canceled")
-			return
-		case <-self.ticker.C:
-		}
-	}
-}
-
-func (self *ProfitTracker) nonceSubmittedSub(output chan *tellor.ITellorNonceSubmitted) (event.Subscription, error) {
-	var tellorFilterer *tellor.ITellorFilterer
-	tellorFilterer, err := tellor.NewITellorFilterer(self.contractInstance.Address, self.client)
-	if err != nil {
-		return nil, errors.Wrap(err, "getting ITellorFilterer instance")
-	}
-	sub, err := tellorFilterer.WatchNonceSubmitted(&bind.WatchOpts{}, output, nil, nil)
-	if err != nil {
-		return nil, errors.Wrap(err, "getting NonceSubmitted channel")
-	}
-	return sub, nil
-}
-
 func (self *ProfitTracker) monitorReward() {
 	var err error
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
-
 	var sub event.Subscription
-	events := make(chan *tellor.ITellorTransferred)
+	events := make(chan *tellor.TellorTransferred)
+
+	logger := log.With(self.logger, "event", "Transfer")
 
 	for {
 		sub, err = self.transferSub(events)
 		if err != nil {
-			level.Error(self.logger).Log("msg", "initial subscribing to Transferred events failed")
+			level.Error(logger).Log("msg", "initial subscribing to events", "err", err)
 			select {
 			case <-ticker.C:
 				continue
@@ -295,9 +170,9 @@ func (self *ProfitTracker) monitorReward() {
 			return
 		case err := <-sub.Err():
 			if err != nil {
-				level.Error(self.logger).Log(
+				level.Error(logger).Log(
 					"msg",
-					"Transferred subscription error",
+					"subscription error",
 					"err", err)
 			}
 
@@ -305,7 +180,7 @@ func (self *ProfitTracker) monitorReward() {
 			for {
 				sub, err = self.transferSub(events)
 				if err != nil {
-					level.Error(self.logger).Log("msg", "re-subscribing to Transferred events failed")
+					level.Error(logger).Log("msg", "re-subscribing to events failed")
 					select {
 					case <-ticker.C:
 						continue
@@ -315,33 +190,200 @@ func (self *ProfitTracker) monitorReward() {
 				}
 				break
 			}
-			level.Info(self.logger).Log("msg", "re-subscribed to Transferred events")
+			level.Info(logger).Log("msg", "re-subscribed to events")
 		case event := <-events:
-			logger := log.With(self.logger, "event", "transfer", "addr", event.To.String()[:6], "tx", event.Raw.TxHash)
+			logger := log.With(logger, "addr", event.To.String()[:6], "tx", event.Raw.TxHash)
 
 			if event.Raw.Removed {
-				trb, err := self.cacheTXsProfit.Get(txID(event))
+				val, err := self.cacheTXsProfit.Get(txIDTransfer(event))
 				if err != nil {
 					level.Error(logger).Log("msg", "getting cache amount for removed event", "err", err)
 					continue
 				}
-				level.Debug(logger).Log("msg", "removed event", "amount", trb.(float64))
-				self.submitProfit.With(prometheus.Labels{"addr": event.To.String()}).(prometheus.Gauge).Sub(trb.(float64))
+				level.Debug(logger).Log("msg", "removed event", "amount", val.(float64))
+				self.submitProfit.With(prometheus.Labels{"addr": event.To.String()}).(prometheus.Gauge).Sub(val.(float64))
 				continue
 			}
 
-			// ctx, cncl := context.WithCancel(self.ctx)
-			// self.cacheTXsProfit.Set(contextID(event), cncl)
-			self.setProfitWhenConfirmed(self.ctx, logger, event)
+			self.setProfitWhenConfirmed(logger, event)
 		}
 	}
 }
 
-func (self *ProfitTracker) transferSub(output chan *tellor.ITellorTransferred) (event.Subscription, error) {
-	var tellorFilterer *tellor.ITellorFilterer
-	tellorFilterer, err := tellor.NewITellorFilterer(self.contractInstance.Address, self.client)
+func (self *ProfitTracker) monitorCost() {
+	var err error
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	logger := log.With(self.logger, "event", "NonceSubmitted")
+
+	var sub event.Subscription
+	events := make(chan *tellor.TellorNonceSubmitted)
+
+	for {
+		sub, err = self.nonceSubmittedSub(events)
+		if err != nil {
+			level.Error(logger).Log("msg", "initial subscribing to events failed")
+			select {
+			case <-ticker.C:
+				continue
+			case <-self.ctx.Done():
+				return
+			}
+		}
+		break
+	}
+
+	for {
+		select {
+		case <-self.ctx.Done():
+			return
+		case err := <-sub.Err():
+			if err != nil {
+				level.Error(logger).Log(
+					"msg",
+					"Transferred subscription error",
+					"err", err)
+			}
+
+			// Trying to resubscribe until it succeeds.
+			for {
+				sub, err = self.nonceSubmittedSub(events)
+				if err != nil {
+					level.Error(logger).Log("msg", "re-subscribing to events failed", "err", err)
+					select {
+					case <-ticker.C:
+						continue
+					case <-self.ctx.Done():
+						return
+					}
+				}
+				break
+			}
+			level.Info(logger).Log("msg", "re-subscribed to events")
+		case event := <-events:
+			logger := log.With(logger, "addr", event.Miner.String()[:6], "tx", event.Raw.TxHash)
+
+			if event.Raw.Removed {
+				val, err := self.cacheTXsCost.Get(txIDNonceSubmit(event))
+				if err != nil {
+					level.Error(logger).Log("msg", "getting cache amount for removed event", "err", err)
+					continue
+				}
+				level.Debug(logger).Log("msg", "removed event", "amount", val.(float64))
+				self.submitCost.With(prometheus.Labels{"addr": event.Miner.String()}).(prometheus.Gauge).Sub(val.(float64))
+				continue
+			}
+
+			self.setCostWhenConfirmed(logger, event)
+		}
+	}
+}
+
+func (self *ProfitTracker) setCostWhenConfirmed(logger log.Logger, event *tellor.TellorNonceSubmitted) {
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+	for {
+		receipt, err := self.client.TransactionReceipt(self.ctx, event.Raw.TxHash)
+		if err != nil {
+			level.Error(logger).Log("msg", "receipt retrieval failed", "err", err)
+		} else if receipt != nil { // Non need to check TX status as even failed TXs cost ETH and need to be recorded.
+			tx, _, err := self.client.TransactionByHash(self.ctx, event.Raw.TxHash)
+			if err != nil {
+				level.Error(logger).Log("msg", "get transaction by hash", "err", err)
+				return
+			}
+			cost, _ := big.NewFloat(0).Mul(big.NewFloat(float64(tx.GasPrice().Int64())), big.NewFloat(float64(receipt.GasUsed))).Float64()
+			cost = cost / 1e18
+			level.Debug(logger).Log("msg", "adding cost", "amount", cost)
+			self.submitCost.With(prometheus.Labels{"addr": event.Miner.String()}).(prometheus.Gauge).Add(cost)
+
+			if err := self.cacheTXsCost.Set(txIDNonceSubmit(event), cost); err != nil {
+				level.Error(logger).Log("msg", "adding amount to the cache", "err", err)
+			}
+
+			balance, err := self.getETHBalance(event.Miner)
+			if err != nil {
+				level.Error(logger).Log("msg", "getting ETH balance", "err", err)
+				return
+			}
+			level.Debug(logger).Log("msg", "new ETH balance", "balance", balance)
+			self.balanceETH.With(prometheus.Labels{"addr": event.Miner.String()}).(prometheus.Gauge).Set(balance)
+			return
+		}
+
+		level.Debug(logger).Log("msg", "transaction not yet mined")
+
+		select {
+		case <-self.ctx.Done():
+			level.Debug(logger).Log("msg", "transaction confirmation check canceled")
+			return
+		case <-ticker.C:
+		}
+	}
+}
+
+func (self *ProfitTracker) setProfitWhenConfirmed(logger log.Logger, event *tellor.TellorTransferred) {
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+	for {
+		receipt, err := self.client.TransactionReceipt(self.ctx, event.Raw.TxHash)
+		if err != nil {
+			level.Error(logger).Log("msg", "receipt retrieval failed", "err", err)
+		} else if receipt != nil {
+			if receipt.Status != types.ReceiptStatusSuccessful {
+				level.Error(logger).Log("msg", "event status not success so no profit added", "status", receipt.Status)
+				return
+			}
+
+			trb, _ := big.NewFloat(float64(event.Value.Int64())).Float64()
+			trb = trb / 1e18
+			level.Debug(logger).Log("msg", "adding profit", "amount", trb)
+			self.submitProfit.With(prometheus.Labels{"addr": event.To.String()}).(prometheus.Gauge).Add(trb)
+
+			if err := self.cacheTXsProfit.Set(txIDTransfer(event), trb); err != nil {
+				level.Error(logger).Log("msg", "adding amount to the cache", "err", err)
+			}
+
+			balance, err := self.getTRBBalance(event.To)
+			if err != nil {
+				level.Error(logger).Log("msg", "getting TRB balance", "err", err)
+				return
+			}
+			level.Debug(logger).Log("msg", "new TRB balance", "balance", balance)
+			self.balanceTRB.With(prometheus.Labels{"addr": event.To.String()}).(prometheus.Gauge).Set(balance)
+			return
+		}
+
+		level.Debug(logger).Log("msg", "transaction not yet mined")
+
+		select {
+		case <-self.ctx.Done():
+			level.Debug(logger).Log("msg", "transaction confirmation check canceled")
+			return
+		case <-ticker.C:
+		}
+	}
+}
+
+func (self *ProfitTracker) nonceSubmittedSub(output chan *tellor.TellorNonceSubmitted) (event.Subscription, error) {
+	var tellorFilterer *tellor.TellorFilterer
+	tellorFilterer, err := tellor.NewTellorFilterer(self.contractInstance.Address, self.client)
 	if err != nil {
-		return nil, errors.Wrap(err, "getting ITellorFilterer instance")
+		return nil, errors.Wrap(err, "getting instance")
+	}
+	sub, err := tellorFilterer.WatchNonceSubmitted(&bind.WatchOpts{}, output, self.addrs, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "getting channel")
+	}
+	return sub, nil
+}
+
+func (self *ProfitTracker) transferSub(output chan *tellor.TellorTransferred) (event.Subscription, error) {
+	var tellorFilterer *tellor.TellorFilterer
+	tellorFilterer, err := tellor.NewTellorFilterer(self.contractInstance.Address, self.client)
+	if err != nil {
+		return nil, errors.Wrap(err, "getting instance")
 	}
 	sub, err := tellorFilterer.WatchTransferred(
 		&bind.WatchOpts{},
@@ -352,7 +394,7 @@ func (self *ProfitTracker) transferSub(output chan *tellor.ITellorTransferred) (
 		self.addrs,
 	)
 	if err != nil {
-		return nil, errors.Wrap(err, "getting Transferred channel")
+		return nil, errors.Wrap(err, "getting channel")
 	}
 	return sub, nil
 }
@@ -417,6 +459,10 @@ func (self *ProfitTracker) getETHBalance(addr common.Address) (float64, error) {
 	return balanceH, nil
 }
 
-func txID(event *tellor.ITellorTransferred) string {
+func txIDTransfer(event *tellor.TellorTransferred) string {
 	return event.Raw.TxHash.String() + event.To.String()
+}
+
+func txIDNonceSubmit(event *tellor.TellorNonceSubmitted) string {
+	return event.Raw.TxHash.String() + event.Miner.String()
 }
